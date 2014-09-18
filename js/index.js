db027b62827164485ae5231b6625e720ee20d017
Kevin Utecht <kevin@kutecht.com>
2014-09-18 13:20:43 -0500

    Cleanup

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ $(function() {
     location: 'Osseo, MN',
     unit: 'f',
     success: function(weather) {
-      html = '<li>' +weather.temp+ ' &deg;' +weather.units.temp+' [high: '+weather.high+']<br>  '+weather.currently+' - '+weather.text+'<br>wind: '+weather.wind.speed+' '+weather.units.speed+' '+weather.wind.direction+'</li>';
+      html = '<p>' +weather.temp+ ' &deg;' +weather.units.temp+' ['+weather.low+'/'+weather.high+'] '+weather.currently+' - '+weather.text+' [wind: '+weather.wind.speed+' '+weather.units.speed+' '+weather.wind.direction+']</p>';
 
       html += '<p></p>';
 
       
       for(var i=1;i<weather.forecast.length;i++) {
-        html += '<li>'+weather.forecast[i].day+':  ['+weather.forecast[i].low+'/'+weather.forecast[i].high+'] ' +weather.forecast[i].text+ '</li>';
+        html += '<p>'+weather.forecast[i].day+':  ['+weather.forecast[i].low+'/'+weather.forecast[i].high+'] ' +weather.forecast[i].text+ '</p>';
         html += '<p></p>';
       }
       
