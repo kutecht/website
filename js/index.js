0c22c1760491dfd5fe48c7f5ecffa1fa0dc9a018
Kevin Utecht <kevin@kutecht.com>
2014-10-14 12:28:38 -0500

    Cleanup

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ $(function() {
     location: 'Osseo, MN',
     unit: 'f',
     success: function(weather) {
-      html = '<br><br><br><p><center>' +weather.temp+ ' &deg;' +weather.units.temp+' ['+weather.low+'/'+weather.high+'] '+weather.currently+' - '+weather.text+' [wind: '+weather.wind.speed+' '+weather.units.speed+' '+weather.wind.direction+']</center></p>';
-      html += '<br><br>';
+      html = '<br><br><br><p><center>' +weather.temp+ ' &deg;' +weather.units.temp+ ' ('+weather.low+' - '+weather.high+') </center></p>';
+      html += '<p><center>'+weather.currently+' - '+weather.text+' - Wind '+weather.wind.speed+' '+weather.units.speed+' '+weather.wind.direction+'</center></p>';
       
-      html += '<p><center>'+weather.forecast[1].day+':  ['+weather.forecast[1].low+'/'+weather.forecast[1].high+'] ' +weather.forecast[1].text+'</center></p>';
+      html += '<p><center>Tomorrow ('+weather.forecast[1].low+' - '+weather.forecast[1].high+') ' +weather.forecast[1].text+'</center></p>';
       html += '<p></p>';
 
       $("#weather").html(html);
