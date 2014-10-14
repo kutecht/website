24fa2f73fcfe18e08f2e6a924fdce0889343ac51
Kevin Utecht <kevin@kutecht.com>
2014-10-14 16:23:36 -0500

    Added background image

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ $(function() {
     location: 'Osseo, MN',
     unit: 'f',
     success: function(weather) {
-      html = '<br><br><br><p><center>' +weather.temp+ ' &deg;' +weather.units.temp+ ' ('+weather.low+' - '+weather.high+') </center></p>';
+      html = '<br><br><br><center><i class="icon-'+weather.code+'"></i></center></p>';
+      html += '<br><br><p><center>' +weather.temp+ ' &deg;' +weather.units.temp+ ' ('+weather.low+' - '+weather.high+') </center></p>';
       html += '<p><center>'+weather.currently+' - '+weather.text+' - Wind '+weather.wind.speed+' '+weather.units.speed+' '+weather.wind.direction+'</center></p>';
       
       html += '<p><center>Tomorrow ('+weather.forecast[1].low+' - '+weather.forecast[1].high+') ' +weather.forecast[1].text+'</center></p>';
